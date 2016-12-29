29a66a85709c8e6f0b7451817564da5c8d7034db
Alexey Tourbin <alexey.tourbin@gmail.com>
2016-12-29 11:31:29 +0300

    frenc.c: SSE2-optimized lcp()

--- frenc.c
+++ frenc.c
@@ -6,12 +6,38 @@
 #include <endian.h>
 #include "frenc.h"
 
+#ifdef __SSE2__
+#include <emmintrin.h>
+#include <strings.h>
+#endif
+
 // the longest common prefix
-static inline size_t lcp(const char *s1, const char *s2)
+static inline size_t lcp(const char *s1, size_t len1,
+			 const char *s2, size_t len2)
 {
     const char *s0 = s1;
-    while (*s1 && *s1 == *s2)
-	s1++, s2++;
+#ifdef __SSE2__
+    size_t minlen = len1 < len2 ? len1 : len2;
+    size_t xmmsize = minlen / 16;
+    for (size_t i = 0; i < xmmsize; i++) {
+	__m128i xmm1 = _mm_loadu_si128((__m128i *) s1);
+	__m128i xmm2 = _mm_loadu_si128((__m128i *) s2);
+	xmm1 = _mm_cmpeq_epi8(xmm1, xmm2);
+	unsigned mask = _mm_movemask_epi8(xmm1);
+	if (mask != 0xffff) {
+	    s1 += ffs(~mask) - 1;
+	    return s1 - s0;
+	}
+	s1 += 16, s2 += 16;
+    }
+#endif
+    // when lengths are different, there's no need to test for EOL
+    if (len1 != len2)
+	while (*s1 == *s2)
+	    s1++, s2++;
+    else
+	while (*s1 && *s1 == *s2)
+	    s1++, s2++;
     return s1 - s0;
 }
 
@@ -33,14 +59,16 @@ int frenc(char *v[], int n, char **enc)
 {
     assert(n > 0);
     // first pass, compute the total size
-    size_t total = strlen(v[0]) + 1;
+    size_t len1 = strlen(v[0]);
+    size_t total = len1 + 1;
     // preprocessing, improves speed but takes more memory
     int *pplen = malloc(n * sizeof(int));
     if (pplen == NULL)
 	return -2;
     size_t olen = 0;
     for (int i = 1; i < n; i++) {
-	size_t len = lcp(v[i-1], v[i]);
+	size_t len2 = strlen(v[i]);
+	size_t len = lcp(v[i-1], len1, v[i], len2);
 	assert(len <= INT_MAX);
 	pplen[i] = len;
 	int diff = (int) len - (int) olen;
@@ -61,8 +89,9 @@ int frenc(char *v[], int n, char **enc)
 	    }
 	    total += 3;
 	}
-	total += strlen(v[i] + len) + 1;
+	total += len2 - len + 1;
 	olen = len;
+	len1 = len2;
     }
     assert(total <= INT_MAX);
     if (enc == NULL) {
